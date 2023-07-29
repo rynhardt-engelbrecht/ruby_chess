11b513e988582961cdba3fa1388af88aba2de7db
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-07-29 21:23:33 +0200

    feat: add Movable#vertical_movement
    Added the Movable#vertical_movement method to handle movement in a
    vertical line.

--- lib/movement/movable.rb
+++ lib/movement/movable.rb
@@ -6,6 +6,10 @@ module Movable
     backwards_horizontal.concat(forwards_horizontal)
   end
 
+  def vertical_movement(board, rank, file)
+    downwards_vertical.concat(upwards_vertical)
+  end
+
   private
 
   def backwards_horizontal(board, rank, file)
@@ -14,12 +18,11 @@ module Movable
     new_file = file
 
     loop do
-      break unless valid_move?(board, new_rank, new_file)
-
       new_rank -= 1
       new_file -= 1
+      break unless valid_move?(board, new_rank, new_file)
 
-      moves << [new_rank, new_file] if board.data[new_rank][new_file]
+      moves << [new_rank, new_file]
 
       break if board.data[new_rank][new_file]&.color != color
       # when path is blocked off by an enemy piece
@@ -32,15 +35,48 @@ module Movable
     new_file = file
 
     loop do
-      break unless valid_move?(board, new_rank, new_file)
-
       new_rank += 1
       new_file += 1
+      break unless valid_move?(board, new_rank, new_file)
 
-      moves << [new_rank, new_file] if board.data[new_rank][new_file]
+      moves << [new_rank, new_file]
 
       break if board.data[new_rank][new_file]&.color != color
       # when path is blocked off by an enemy piece
+    end
+  end
+
+  def downwards_vertical(board, rank, file)
+    moves = []
+    new_rank = rank
+    new_file = file
+
+    loop do
+      new_rank += 1
+      new_file += 1
+      break unless valid_move?(board, new_rank, new_file)
+
+      moves << [new_rank, new_file]
+
+      break if board.data[new_rank][new_file]&.color != color
+      # when path is blocked off by an enemy piece
+    end
+
+    def upwards_vertical(board, rank, file)
+      moves = []
+      new_rank = rank
+      new_file = file
+
+      loop do
+        new_rank -= 1
+        new_file -= 1
+        break unless valid_move?(board, new_rank, new_file)
+
+        moves << [new_rank, new_file]
+
+        break if board.data[new_rank][new_file]&.color != color
+        # when path is blocked off by an enemy piece
+      end
     end
   end
 end
