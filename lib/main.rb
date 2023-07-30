0f58d829783edc05f8adde3bf7616e73cc569587
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-07-30 15:16:17 +0200

    feat: add main.rb file

---
+++ lib/main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'pieces/bishop'
+require_relative 'pieces/king'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
