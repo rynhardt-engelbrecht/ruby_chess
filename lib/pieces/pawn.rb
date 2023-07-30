88b9ed8e2e377c54f42a8a938655c6dcd931902e
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-07-30 17:06:59 +0200

    feat: add Pawn#generate_moves method
    Added the Pawn#generate_moves method which will generate the pawn
    piece's available moves based on it's location, whether it has
    previously moved or not, and whether there is an opponent piece
    diagonally across it.
    
    Yet to add en_passant possibilities.

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,4 +3,43 @@
 require_relative 'piece'
 
 # contains logic for the Pawn < Piece chess piece
-class Pawn < Piece; end
+class Pawn < Piece
+  def initialize(board, location, color)
+    super
+    @direction = @color == :white ? -1 : 1
+  end
+
+  def generate_moves(board, rank, file)
+    moveset.each do |move|
+      new_rank = rank + move[0]
+      new_file = file + move[1]
+
+      break unless board[new_rank][new_file].nil?
+
+      @valid_moves << [new_rank, new_file] if valid_move?(board, new_rank, new_file)
+    end
+
+    check_captures(board, rank, file)
+  end
+
+  private
+
+  def check_captures(board, rank, file)
+    capture_moves.each do |move|
+      capture_rank = rank + move[0]
+      capture_file = file + move[1]
+
+      @valid_captures << [capture_rank, capture_file] if opponent_piece?(board, capture_rank, capture_file)
+    end
+  end
+
+  def moveset
+    return [[@direction, 0], [@direction * 2, 0]] unless @moved
+
+    [[@direction, 0]] if @moved
+  end
+
+  def capture_moves
+    [[@direction, 1], [@direction, -1]]
+  end
+end
