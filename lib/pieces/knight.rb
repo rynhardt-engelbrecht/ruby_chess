89cde96802d5b84d817ac34233a436845956a7e4
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-07-30 14:50:18 +0200

    feat: add Knight#generate_moves
    Added the Knight#generate_moves method which loops through an array
    returned by the Knight#moveset private method, and filters out illegal
    moves.

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -3,4 +3,32 @@
 require_relative 'piece'
 
 # contains logic for the Knight chess piece
-class Knight < Piece; end
+class Knight < Piece
+  def generate_moves(board, rank, file)
+    moves = []
+
+    moveset.each do |move|
+      new_rank = rank + move[0]
+      new_file = file + move[1]
+
+      moves << [new_rank, new_file] if valid_move?(board, rank, file)
+    end
+
+    moves
+  end
+
+  private
+
+  def moveset
+    [
+      [-2, -1],
+      [-2, 1],
+      [-1, -2],
+      [-1, 2],
+      [1, -2],
+      [1, 2],
+      [2, -1],
+      [2, 1]
+    ]
+  end
+end
