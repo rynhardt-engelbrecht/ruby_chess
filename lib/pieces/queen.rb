8855cf256c7ba98c4d37656c89dca0e0adc30f05
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-07-30 16:06:57 +0200

    feat: add Queen#generate_moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -6,4 +6,18 @@ require_relative '../movement/movable'
 # contains logic for the Queen < Piece chess piece
 class Queen < Piece
   include Movable
+
+  def generate_moves(board, rank, file)
+    move_array = horizontal_movement(board, rank, file).concat(
+      vertical_movement(board, rank, file),
+      diagonal_movement(board, rank, file)
+    )
+
+    move_array.each do |move|
+      new_rank = move[0]
+      new_file = move[1]
+
+      add_move(board, [new_rank, new_file]) if valid_move?(board, new_rank, new_file)
+    end
+  end
 end
