29155981424c3b185128e776e896095fa9aa2f57
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-08-01 18:52:50 +0200

    refactor: update messages

--- lib/text/text_output.rb
+++ lib/text/text_output.rb
@@ -4,9 +4,9 @@
 module TextOutput
   def turn_message(message, color = nil)
     {
-      'which color' => " #{color}'s turn \n==============",
-      'square' => 'Enter coordinates of the piece you want to move (Eg. b2)>>',
-      'move' => 'Enter the coordinates of the square you want to move the selected piece to (Eg. c2)>>'
+      'which color' => "==============\n #{color}'s turn \n==============",
+      'square' => 'Enter coordinates of the piece you want to move>>',
+      'move' => 'Enter the coordinates of the square you want to move the selected piece to>>'
     }[message]
   end
 end
