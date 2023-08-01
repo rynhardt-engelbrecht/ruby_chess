3517dfa3de8bca76ec6a12c7dacb1c0a68f06625
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-08-01 12:09:01 +0200

    feat: add Game#setup_game and helper methods

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,22 @@ class Game
   def initialize(mode)
     @mode = mode
   end
+
+  def setup_game(board_obj, first_player_obj, second_player_obj)
+    @board = create_board(board_obj)
+    @players = create_players(@board, first_player_obj, second_player_obj)
+  end
+
+  private
+
+  def create_board(board_obj)
+    @board = board_obj.new
+  end
+
+  def create_players(board, first_player_obj, second_player_obj)
+    [
+      first_player_obj.new(board, :white),
+      second_player_obj.new(board, :black)
+    ]
+  end
 end
