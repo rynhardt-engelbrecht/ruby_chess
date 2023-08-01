3cade157c895a12e93fb90ab4809a2009f61550f
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-08-01 18:49:58 +0200

    feat: add Game#play method
    Added the Game#play method along with helper methods

--- lib/game.rb
+++ lib/game.rb
@@ -2,19 +2,42 @@
 
 # contains main logic to run the game
 class Game
+  attr_reader :players, :board, :mode
+
   def initialize(mode)
     @mode = mode
+    @active = true
+  end
+
+  def play
+    fullmove until @active == false
   end
 
   def setup_game(board_obj, first_player_obj, second_player_obj)
-    @board = create_board(board_obj)
+    create_board(board_obj)
     @players = create_players(@board, first_player_obj, second_player_obj)
   end
 
   private
 
+  def fullmove
+    halfmove
+    halfmove
+  end
+
+  def halfmove
+    @board.print_board
+
+    active_player = players.find { |player| player.color == @board.active_color }
+    active_player.turn
+    @board.print_board
+
+    @board.active_color = @board.active_color == :white ? :black : :white
+  end
+
   def create_board(board_obj)
     @board = board_obj.new
+    @board.piece_setup
   end
 
   def create_players(board, first_player_obj, second_player_obj)
