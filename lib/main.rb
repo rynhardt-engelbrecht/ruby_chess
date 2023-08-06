f3f9001eac9472bcf6c21ee1dd627f271a4bf694
Rynhardt Engelbrecht <107496937+rynhardt-engelbrecht@users.noreply.github.com>
2023-08-06 20:30:09 +0200

    refactor: move player.rb file into own directory

--- lib/main.rb
+++ lib/main.rb
@@ -10,6 +10,6 @@ require_relative 'player'
 require_relative 'game'
 require_relative 'serializer'
 
-# game = Game.new(0)
-# game.setup_game(Board, Player, Player)
-# game.play
+game = Game.new(0)
+game.setup_game(Board, Player, Player)
+game.play
